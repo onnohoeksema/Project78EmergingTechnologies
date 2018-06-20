1fb731411288a67db9f5ff0f4a4b48e72f372f0f
Ricardo van Oldenbarneveld genaamd Witte Tullingh <r.vanoldenbarneveld@live.nl>
2018-06-20 21:40:30 +0100

    2nd project has been added in order to include Windows Forms

---
+++ ConsoleApplication1/CLR1/StartScreen.cpp
@@ -0,0 +1,12 @@
+#include "StartScreen.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	CLR1::StartScreen form;
+	Application::Run(%form);
+}
